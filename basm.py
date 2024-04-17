554adb1018720ea503f82746ebdc71dbe96183b1
Matthias Wijnsma <matthiasx95@gmail.com>
2024-04-17 15:13:41 +0200

    add checks and generate debug info

--- basm.py
+++ basm.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 labels = {}
 
 def get_current_address() -> int:
@@ -5,70 +8,105 @@ def get_current_address() -> int:
 
 def assemble(opcode: str, args: list[str]) -> int:
     if opcode == "ldi":
+        assert_amount_of_args(args, 2)
         return 0b1010 << 12 | assemble_reg(args[0]) << 9 | assemble_immediate(args[1])
     elif opcode == "jmp":
+        assert_amount_of_args(args, 1)
         return 0b0010 << 12 | assemble_address(args[0])
     elif opcode == "nop":
+        assert_amount_of_args(args, 0)
         return 0b0000 << 12
     elif opcode == "hlt":
+        assert_amount_of_args(args, 0)
         return 0b0001 << 12
     elif opcode == "brh":
+        assert_amount_of_args(args, 2)
         return 0b0011 << 12 | assemble_condition(args[0]) << 10 | assemble_address(args[1])
     elif opcode == "cal":
+        assert_amount_of_args(args, 1)
         return 0b0100 << 12 | assemble_address(args[0])
     elif opcode == "ret":
+        assert_amount_of_args(args, 0)
         return 0b0101 << 12
     elif opcode == "pld":
+        assert_amount_of_args(args, 2)
         return 0b0110 << 12 | assemble_reg(args[0]) << 9 | assemble_immediate(args[1], port=True)
     elif opcode == "pst":
+        assert_amount_of_args(args, 2)
         return 0b0111 << 12 | assemble_reg(args[0]) << 9 | assemble_immediate(args[1], port=True)
     elif opcode == "mld":
+        assert_amount_of_args(args, 3)
         return 0b1000 << 12 | assemble_reg(args[0]) << 9 | assemble_reg(args[1]) << 6 | assemble_offset(args[2])
     elif opcode == "mst":
+        assert_amount_of_args(args, 3)
         return 0b1001 << 12 | assemble_reg(args[0]) << 9 | assemble_reg(args[1]) << 6 | assemble_offset(args[2])
     elif opcode == "adi":
+        assert_amount_of_args(args, 2)
         return 0b1011 << 12 | assemble_reg(args[0]) << 9 | assemble_immediate(args[1])
     elif opcode == "add":
+        assert_amount_of_args(args, 3)
         return 0b1100 << 12 | assemble_reg(args[0]) << 9 | assemble_reg(args[1]) << 6 | assemble_reg(args[2])
     elif opcode == "sub":
+        assert_amount_of_args(args, 3)
         return 0b1101 << 12 | assemble_reg(args[0]) << 9 | assemble_reg(args[1]) << 6 | assemble_reg(args[2])
     elif opcode == "bit":
+        assert_amount_of_args(args, 4)
         return 0b1110 << 12 | assemble_reg(args[0]) << 9 | assemble_reg(args[1]) << 6 | assemble_operation(args[2]) << 3 | assemble_reg(args[3])
     elif opcode == "rsh":
+        assert_amount_of_args(args, 2)
         return 0b1111 << 12 | assemble_reg(args[0]) << 9 | assemble_reg(args[1]) << 6
     # psuedo-instructions
     elif opcode == "cmp":
+        assert_amount_of_args(args, 2)
         return assemble("sub", ["r0", *args])
     elif opcode == "mov":
+        assert_amount_of_args(args, 2)
         return assemble("add", [*args, "r0"])
     elif opcode == "lsh":
+        assert_amount_of_args(args, 2)
         return assemble("add", [*args, args[1]])
     elif opcode == "inc":
+        assert_amount_of_args(args, 1)
         return assemble("adi", [*args, "1"])
     elif opcode == "dec":
+        assert_amount_of_args(args, 1)
         return assemble("adi", [*args, "-1"])
     elif opcode == "orr":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "or", args[2]])
     elif opcode == "and":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "and", args[2]])
     elif opcode == "xor":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "xor", args[2]])
     elif opcode == "imp":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "implies", args[2]])
     elif opcode == "nor":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "nor", args[2]])
     elif opcode == "nnd":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "nand", args[2]])
     elif opcode == "xnr":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "xnor", args[2]])
     elif opcode == "nmp":
+        assert_amount_of_args(args, 3)
         return assemble("bit", [args[0], args[1], "nimplies", args[2]])
     elif opcode == "not":
+        assert_amount_of_args(args, 2)
         return assemble("bit", [args[0], args[1], "nor", "r0"])
     else:
         print(f"{input_path}:{line_number}: unknown opcode {opcode!r}")
         exit(1)
-    
+
+def assert_amount_of_args(args: list[str], amount: int) -> None:
+    if len(args) != amount:
+        print(f"{input_path}:{line_number}: expected {amount} argument(s), got {len(args)}")
+        exit(1)
+
 def assemble_operation(op: str) -> int:
     if op == "or":
         return 0b000
@@ -185,7 +223,7 @@ def assemble_reg(reg: str) -> int:
 
     return reg_num
 
-def assemble_file(_input_path: str, _output_path: str) -> None:
+def assemble_file(_input_path: str, _output_path: str, debug: bool) -> None:
     global line_number
     global machine_code
     global input_path
@@ -196,6 +234,11 @@ def assemble_file(_input_path: str, _output_path: str) -> None:
 
     with open(input_path, "r") as f:
         source = f.read()
+
+    debug_info = {
+        "labels": {},
+        "instructions": []
+    }
 
     machine_code = b""
 
@@ -216,6 +259,11 @@ def assemble_file(_input_path: str, _output_path: str) -> None:
                 print(f"{input_path}:{line_number}: label {label!r} already defined")
                 exit(1)
             labels[label] = get_current_address()
+
+            debug_info["labels"][label] = {
+                "line": line_number,
+                "address": get_current_address()
+            }
 
         # ignore empty lines
         if not line.strip():
@@ -246,22 +294,52 @@ def assemble_file(_input_path: str, _output_path: str) -> None:
             continue
 
         opcode, *args = line.split(maxsplit=1)
-        args = args[0] if args else ""
-        args = [arg.strip() for arg in args.split(",")]
+        if args:
+            args = [arg.strip() for arg in args[0].split(",")]
+        else:
+            args = []
         if any([" " in arg for arg in args]):
             print(f"{input_path}:{line_number}: missing comma between arguments")
             exit(1)
 
+        debug_info["instructions"].append({
+            "line": line_number,
+            "address": get_current_address(),
+        })
+
         machine_code += assemble(opcode, args).to_bytes(2, "big")
+
+    if debug:
+        debug_info["source"] = source
+
+        with open(f"{output_path}.dbg", "w") as f:
+            json.dump(debug_info, f, indent=4)
 
     with open(output_path, "wb") as f:
         f.write(machine_code)
 
-if __name__ == "__main__":
-    import sys
+def print_help(error: bool) -> None:
+    print(f"usage: {sys.argv[0]} [flags] <input.basm> <output.bin>")
+    print("flags:")
+    print("  -h, --help: show this help message")
+    print("  -d, --debug: generate debug information (for use with bdbg)")
 
-    if len(sys.argv) != 3:
-        print(f"usage: {sys.argv[0]} input.basm output.bin")
-        exit(1)
-    
-    assemble_file(sys.argv[1], sys.argv[2])
+    exit(1 if error else 0)
+
+if __name__ == "__main__":
+    debug = False
+    while len(sys.argv) > 1 and sys.argv[1].startswith("-"):
+        if sys.argv[1] in ["-h", "--help"]:
+            print_help(error=False)
+        elif sys.argv[1] in ["-d", "--debug"]:
+            debug = True
+        else:
+            print(f"unknown flag {sys.argv[1]!r}")
+            print_help(error=True)
+
+        sys.argv.pop(1)
+
+    if len(sys.argv) < 3:
+        print_help(error=True)
+
+    assemble_file(sys.argv[1], sys.argv[2], debug)
