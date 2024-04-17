b1037ff268012c96e405b5f7b4548db4327d1656
Matthias Wijnsma <matthiasx95@gmail.com>
2024-04-17 11:56:25 +0200

    slight refactor

--- basm.py
+++ basm.py
@@ -1,7 +1,3 @@
-with open("test.basm", "r") as f:
-    source = f.read()
-
-machine_code = b""
 labels = {}
 
 def get_current_address() -> int:
@@ -70,7 +66,7 @@ def assemble(opcode: str, args: list[str]) -> int:
     elif opcode == "not":
         return assemble("bit", [args[0], args[1], "nor", "r0"])
     else:
-        print(f"test.basm:{line_number}: unknown opcode {opcode!r}")
+        print(f"{input_path}:{line_number}: unknown opcode {opcode!r}")
         exit(1)
     
 def assemble_operation(op: str) -> int:
@@ -91,7 +87,7 @@ def assemble_operation(op: str) -> int:
     elif op == "nimplies":
         return 0b111
     else:
-        print(f"test.basm:{line_number}: unknown operation {op!r}")
+        print(f"{input_path}:{line_number}: unknown operation {op!r}")
         exit(1)
 
 def assemble_condition(cond: str) -> int:
@@ -104,7 +100,7 @@ def assemble_condition(cond: str) -> int:
     elif cond == "lt":
         return 0b11
     else:
-        print(f"test.basm:{line_number}: unknown condition {cond!r}")
+        print(f"{input_path}:{line_number}: unknown condition {cond!r}")
         exit(1)
 
 def assemble_offset(offset: str) -> int:
@@ -112,11 +108,11 @@ def assemble_offset(offset: str) -> int:
     try:
         offset_int = int(offset)
     except ValueError:
-        print(f"test.basm:{line_number}: expected offset, got {offset!r}")
+        print(f"{input_path}:{line_number}: expected offset, got {offset!r}")
         exit(1)
 
     if offset_int < -32 or offset_int > 31:
-        print(f"test.basm:{line_number}: offset must be between -32 and 31")
+        print(f"{input_path}:{line_number}: offset must be between -32 and 31")
         exit(1)
 
     return offset_int & 0x3f
@@ -126,12 +122,12 @@ def assemble_address(addr: str) -> int:
         addr_int = int(addr)
     except ValueError:
         if addr not in labels:
-            print(f"test.basm:{line_number}: unknown label or invalid integer address {addr!r}")
+            print(f"{input_path}:{line_number}: unknown label or invalid integer address {addr!r}")
             exit(1)
         addr_int = labels[addr]
 
     if addr_int < 0 or addr_int >= 2**10:
-        print(f"test.basm:{line_number}: address must be between 0 and 1023")
+        print(f"{input_path}:{line_number}: address must be between 0 and 1023")
         exit(1)
 
     return addr_int
@@ -144,82 +140,105 @@ def assemble_immediate(imm: str, port: bool = False) -> int:
     try:
         imm_int = int(imm)
     except ValueError:
-        print(f"test.basm:{line_number}: expected {expected}, got {imm!r}")
+        print(f"{input_path}:{line_number}: expected {expected}, got {imm!r}")
         exit(1)
 
     if imm_int < -128 or imm_int > 255:
-        print(f"test.basm:{line_number}: {expected} must be between -128 and 255")
+        print(f"{input_path}:{line_number}: {expected} must be between -128 and 255")
         exit(1)
 
     return imm_int & 0xff
 
 def assemble_reg(reg: str) -> int:
     if reg[0] != "r" or not reg[1:].isdigit():
-        print(f"test.basm:{line_number}: expected register, got {reg!r}")
+        print(f"{input_path}:{line_number}: expected register, got {reg!r}")
         exit(1)
 
     reg_num = int(reg[1:])
 
     if reg_num >= 8:
-        print(f"test.basm:{line_number}: register number must be less than 8")
+        print(f"{input_path}:{line_number}: register number must be less than 8")
         exit(1)
 
     return reg_num
 
-# first pass: process labels
-line_number = 0
-for line in source.splitlines():
-    line_number += 1
+def assemble_file(_input_path: str, _output_path: str) -> None:
+    global line_number
+    global machine_code
+    global input_path
+    global output_path
 
-    # remove comment
-    line = line.split("#")[0].strip()
+    input_path = _input_path
+    output_path = _output_path
 
-    # process labels
-    while ":" in line:
-        label = line.split(":")[0].strip()
-        line = ":".join(line.split(":")[1:]).strip()
+    with open(input_path, "r") as f:
+        source = f.read()
 
-        if label in labels:
-            print(f"test.basm:{line_number}: label {label!r} already defined")
+    machine_code = b""
+
+    # first pass: process labels
+    line_number = 0
+    for line in source.splitlines():
+        line_number += 1
+
+        # remove comment
+        line = line.split("#")[0].strip()
+
+        # process labels
+        while ":" in line:
+            label = line.split(":")[0].strip()
+            line = ":".join(line.split(":")[1:]).strip()
+
+            if label in labels:
+                print(f"{input_path}:{line_number}: label {label!r} already defined")
+                exit(1)
+            labels[label] = get_current_address()
+
+        # ignore empty lines
+        if not line.strip():
+            continue
+
+        machine_code += b"\x00\x00"
+
+    machine_code = b""
+
+    # second pass: assemble instructions
+    line_number = 0
+    for line in source.splitlines():
+        line_number += 1
+
+        if get_current_address() >= 2**10:
+            print(f"{input_path}:{line_number}: program too long")
             exit(1)
-        labels[label] = get_current_address()
 
-    # ignore empty lines
-    if not line.strip():
-        continue
+        # remove comment
+        line = line.split("#")[0].strip()
 
-    machine_code += b"\x00\x00"
+        # remove labels
+        while ":" in line:
+            line = ":".join(line.split(":")[1:]).strip()
 
-machine_code = b""
+        # ignore empty lines
+        if not line.strip():
+            continue
 
-# second pass: assemble instructions
-line_number = 0
-for line in source.splitlines():
-    line_number += 1
+        opcode, *args = line.split(maxsplit=1)
+        args = args[0] if args else ""
+        args = [arg.strip() for arg in args.split(",")]
+        if any([" " in arg for arg in args]):
+            print(f"{input_path}:{line_number}: missing comma between arguments")
+            exit(1)
 
-    if get_current_address() >= 2**10:
-        print(f"test.basm:{line_number}: program too long")
+        machine_code += assemble(opcode, args).to_bytes(2, "big")
+
+    with open(output_path, "wb") as f:
+        f.write(machine_code)
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 3:
+        print(f"usage: {sys.argv[0]} input.asm output.bin")
         exit(1)
-
-    # remove comment
-    line = line.split("#")[0].strip()
-
-    # remove labels
-    while ":" in line:
-        line = ":".join(line.split(":")[1:]).strip()
-
-    # ignore empty lines
-    if not line.strip():
-        continue
-
-    opcode, *args = line.split(maxsplit=1)
-    args = args[0] if args else ""
-    args = [arg.strip() for arg in args.split(",")]
-    if any([" " in arg for arg in args]):
-        print(f"test.basm:{line_number}: missing comma between arguments")
-        exit(1)
-
-    machine_code += assemble(opcode, args).to_bytes(2, "big")
-
-with open("test.bin", "wb") as f:
-    f.write(machine_code)
+    
+    assemble_file(sys.argv[1], sys.argv[2])
